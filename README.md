6d42e099db2560f320951c724d8cf8b27592a651
Niall <nimaster@cisco.com>
2021-11-17 14:09:28 +0000

    added README file

---
+++ README.md
@@ -0,0 +1,5 @@
+# BGP ROUTE-POLICY CHECK
+
+The get-rpl-and-check.py script gathers the route-policy IN/OUT for each BGP peer and verify if the route-policy name conforms to the standard:
+route-policy out should end with the characters "-OUT"
+route-policy in should end with the characters "-IN"
--- test.py
+++
@@ -1,7 +0,0 @@
-
-bla = "[OrderedDict([('neighbor-group-name', 'ISP'), ('neighbor-group-afs', OrderedDict([('neighbor-group-af', OrderedDict([('af-name', 'ipv4-unicast'), ('route-policy-in', 'BGP-IN'), ('route-policy-out', 'BGP-OUT')]))]))]), OrderedDict([('neighbor-group-name', 'IBGP'), ('neighbor-group-afs', OrderedDict([('neighbor-group-af', OrderedDict([('af-name', 'ipv4-unicast'), ('route-policy-in', 'ACCEPT-ALL'), ('route-policy-out', 'PASS')]))]))]), OrderedDict([('neighbor-group-name', 'TEST'), ('neighbor-group-afs', OrderedDict([('neighbor-group-af', OrderedDict([('af-name', 'ipv4-unicast'), ('route-policy-in', 'drop'), ('route-policy-out', 'ACCEPT-ALL')]))]))])]"
-
-print(bla)
-
-print(bla.count('neighbor-group-name'))
-
